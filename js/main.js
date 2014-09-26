a7b6502813291c013aa02ed759dbf4ba0c1d0999
James Curran <james@it.usyd.edu.au>
2014-09-26 22:47:08 +1000

    Hacked webdis protocol to work around publishing fails.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 
-lectour = {};
+lectour = {
+  presenter: {},
+  projector: {},
+  audience: {}
+};
 
-lectour.PRESENTER = false;
-// lectour.URL = 'ws://ec2-54-252-243-156.ap-southeast-2.compute.amazonaws.com:9379/.json';
-lectour.URL = 'ws://0.0.0.0:9379/';
+lectour.PRESENTER = document.URL.indexOf('presenter') !== -1;
+lectour.CHANNEL = lectour.PRESENTER ? 'presenter' : 'audience';
+lectour.SERVER = 'ec2-54-252-243-156.ap-southeast-2.compute.amazonaws.com:9379';
 lectour.ws = null;
 
 lectour.init = function() {
-  lectour.connect(lectour.URL);
+  lectour.connect(lectour.SERVER);
 
   $('.step').on('enterStep', function(event) {
     console.log('enterStep', event);
     if (lectour.PRESENTER) {
-      lectour.goto('#' + event.target.id);
+      lectour.presenter.goto(event.target.id);
     }
   });
 }
 
-lectour.connect = function(url) {
-  lectour.ws = new WebSocket(url);
+lectour.connect = function(server) {
+  lectour.ws = new WebSocket('ws://' + server + '/');
   lectour.ws.onopen = function() {
-    lectour.subscribe('events');
+    lectour.send(["SUBSCRIBE", lectour.CHANNEL]);
   };
 
   lectour.ws.onmessage = function(event) {
@@ -33,33 +37,33 @@ lectour.send = function(data) {
   lectour.ws.send(JSON.stringify(data));
 }
 
-lectour.subscribe = function(channel) {
-  console.log('subscribe', arguments);
-  lectour.send(["SUBSCRIBE", channel]);
-}
-
-lectour.publish = function(channel, msg) {
-  console.log('publish', arguments);
-  lectour.send(['PUBLISH', channel, msg]);
+lectour.pub = function(channel, data) {
+  $.ajax({
+    url: "http://" + lectour.SERVER + '/PUBLISH/' + channel,
+    type: "PUT",
+    data: data,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json"
+  });
 }
 
 lectour.dispatch = function(msg) {
   if (msg.SUBSCRIBE !== undefined) {
     msg = msg.SUBSCRIBE;
     if (msg[0] === "message") {
-      if (msg[1] === "events") {
-        lectour.onGoto(msg[2]);
+      if (msg[1] === "audience") {
+        lectour.audience.onGoto(msg[2]);
       }
     }
   }
 }
 
-lectour.goto = function(slide) {
+lectour.presenter.goto = function(slide) {
   console.log('goto', arguments);
-  lectour.publish('events', slide);
+  lectour.pub('audience', slide);
 }
 
-lectour.onGoto = function(slide) {
+lectour.audience.onGoto = function(slide) {
   console.log('onGoto', arguments);
-  $('#slides').jmpress('select', slide);
+  $('#slides').jmpress('select', '#' + slide);
 }
