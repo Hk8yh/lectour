4aa7dc4b981b4d57db747f9998d28a6875063f7d
James Curran <james@it.usyd.edu.au>
2014-09-27 07:42:16 +1000

    Added node.js bridge between Redis and web sockets (using socket.io). Socket.io handles JSON and dispatching much better than the webdis etc. Bridge also handles notifications to one or more pubsub channels and transmits the updated value. All gets are now handled as get "prompt" request, followed by an async update message back. Added handlers to node.js web server to server the HTML files as well. Added some basic examples for using the lectour API.

---
+++ js/lectour.js
@@ -0,0 +1,64 @@
+
+lectour = {
+  presenter: {},
+  projector: {},
+  audience: {}
+};
+
+lectour.socket = null;
+
+lectour.init = function(mode, el) {
+  lectour.mode = mode;
+  lectour.socket = io();
+
+  lectour.sub(mode);
+
+  lectour.socket.on('do', function(channel, cmd, data) {
+    $(el).trigger('lt:' + channel + ':' + cmd, data);
+  });
+
+  lectour.socket.on('up', function(sel, type, item, value) {
+    console.log('update', sel, type, item, value);
+    $(sel).trigger('lt:up:' + type, item, value);
+  });
+}
+
+lectour.send = function() {
+  lectour.socket.emit.apply(lectour.socket, arguments);
+}
+
+lectour.sub = function(channels) {
+  lectour.send('sub', channels);
+}
+
+lectour.pub = function(channels, cmd, data) {
+  lectour.send('pub', channels, cmd, data);
+}
+
+lectour.get = function(sel, type) {
+  lectour.send('get', sel, type);
+}
+
+lectour.set = function(sel, type, value, tell) {
+  lectour.send('set', sel, type, value, tell);
+}
+
+lectour.inc = function(sel, type, tell) {
+  lectour.send('inc', sel, type, tell);
+}
+
+lectour.hget = function(sel, type, item) {
+  lectour.send('hget', sel, type);
+}
+
+lectour.hset = function(sel, type, item, value, tell) {
+  lectour.send('hset', sel, type, item, value, tell);
+}
+
+lectour.hinc = function(sel, type, item, tell) {
+  lectour.send('hinc', sel, type, item, tell);
+}
+
+lectour.hitems = function(sel, type) {
+  lectour.send('hitems', sel, type);
+}
--- js/main.js
+++ js/main.js
@@ -1,69 +1,34 @@
 
-lectour = {
-  presenter: {},
-  projector: {},
-  audience: {}
-};
+$(function() {
+  var mode = document.URL.split('#')[0].split('/').pop();
+  lectour.init(mode, '#slides');
 
-lectour.PRESENTER = document.URL.indexOf('presenter') !== -1;
-lectour.CHANNEL = lectour.PRESENTER ? 'presenter' : 'audience';
-lectour.SERVER = 'ec2-54-252-243-156.ap-southeast-2.compute.amazonaws.com:9379';
-lectour.ws = null;
-
-lectour.init = function() {
-  lectour.connect(lectour.SERVER);
+	$('#slides').jmpress();
 
   $('.step').on('enterStep', function(event) {
-    console.log('enterStep', event);
-    if (lectour.PRESENTER) {
-      lectour.presenter.goto(event.target.id);
+    if (mode === 'presenter') {
+      lectour.pub(['audience', 'projector'], 'goto', event.target.id);
     }
   });
-}
 
-lectour.connect = function(server) {
-  lectour.ws = new WebSocket('ws://' + server + '/');
-  lectour.ws.onopen = function() {
-    lectour.send(["SUBSCRIBE", lectour.CHANNEL]);
-  };
-
-  lectour.ws.onmessage = function(event) {
-    console.log("JSON received:", event);
-    lectour.dispatch(JSON.parse(event.data));
-  };
-}
-
-lectour.send = function(data) {
-  lectour.ws.send(JSON.stringify(data));
-}
-
-lectour.pub = function(channel, data) {
-  $.ajax({
-    url: "http://" + lectour.SERVER + '/PUBLISH/' + channel,
-    type: "PUT",
-    data: data,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json"
+  $('.help').on('lt:up:help', function(event, item, value) {
+    $(event.target).text(value);
   });
-}
 
-lectour.dispatch = function(msg) {
-  if (msg.SUBSCRIBE !== undefined) {
-    msg = msg.SUBSCRIBE;
-    if (msg[0] === "message") {
-      if (msg[1] === "audience") {
-        lectour.audience.onGoto(msg[2]);
-      }
-    }
-  }
-}
+  $('.help').each(function(i, node) {
+    lectour.get('#' + node.id, 'help');
+  });
 
-lectour.presenter.goto = function(slide) {
-  console.log('goto', arguments);
-  lectour.pub('audience', slide);
-}
+  $('.comment').on('lt:up:comment', function(event, item, value) {
+    console.log('lt:up:comment', item, value);
+  });
 
-lectour.audience.onGoto = function(slide) {
-  console.log('onGoto', arguments);
-  $('#slides').jmpress('select', '#' + slide);
-}
+  $('#slides').on('lt:audience:goto', function(event, slide) {
+    $('#slides').jmpress('goTo', '#' + slide);
+  });
+
+  $('#slides').on('lt:presenter:goto', function(event, slide) {
+    $('#slides').jmpress('goTo', '#' + slide);
+  });
+
+});
